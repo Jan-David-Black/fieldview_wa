464c9773e6161ee51678959abc410b594e35b15a
Jan-David Fischbach <jan-david.fischbach@rwth-aachen.de>
2020-11-29 22:57:25 +0100

    mod paths3

--- overview/index.php
+++ overview/index.php
@@ -1,6 +1,8 @@
 <html lang="en">
 <head>
   <?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'on');
   include_once '../home.php';
   $top_dir = $_SERVER['DOCUMENT_ROOT'].$home;?>
   <?php include $top_dir.'assets/html/head.php'; ?>
